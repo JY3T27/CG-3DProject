6ff8afd0c5c98250c2314aa85eae817e63b96dc9
JY3T27 <121598846+JY3T27@users.noreply.github.com>
2023-06-16 17:44:36 +0800

    Create Basic Human Model.cpp

---
+++ Basic Human Model.cpp
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <gl/gL.h>
+#include <gl/glu.h>
+#include <gl/glut.h>
+#include <math.h>
+
+#define PI 3.141592653
+
+#define SCREEN_WIDTH	500
+#define SCREEN_HEIGHT	500
+#define FPS	30
+
+float angle = 0.0;
+
+void square() {
+	glBegin(GL_QUADS);
+	glVertex3f(1.0, 1.0, 0.0);
+	glVertex3f(-1.0, 1.0, 0.0);
+	glVertex3f(-1.0, -1.0, 0.0);
+	glVertex3f(1.0, -1.0, 0.0);
+	glEnd();
+}
+
+void cube() {
+	glNormal3f(0, 0, 1);
+
+	glPushMatrix();
+	glColor3d(1.0, 0.0, 0.0);
+	glTranslated(0.0, 0.0, 1.0);
+	square();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3d(0.0, 1.0, 0.0);
+	glRotated(90.0, 0.0, 1.0, 0.0);
+	glTranslated(0.0, 0.0, 1.0);
+	square();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3d(1.0, 1.0, 0.0);
+	glRotated(180.0, 0.0, 1.0, 0.0);
+	glTranslated(0.0, 0.0, 1.0);
+	square();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3d(0.0, 1.0, 1.0);
+	glRotated(270.0, 0.0, 1.0, 0.0);
+	glTranslated(0.0, 0.0, 1.0);
+	square();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3d(1.0, 0.0, 1.0);
+	glRotated(-90.0, 1.0, 0.0, 0.0);
+	glTranslated(0.0, 0.0, 1.0);
+	square();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3d(1.0, 1.0, 1.0);
+	glRotated(90.0, 1.0, 0.0, 0.0);
+	glTranslated(0.0, 0.0, 1.0);
+	square();
+	glPopMatrix();
+
+}
+
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+		glColor3f(0.95f, 0.75f, 0.6f);
+		glScaled(1.0, 1.0, 1.0);
+		glTranslated(0.0, 2.0, 0.0);
+		glutSolidCube(2.0);
+	glPopMatrix();
+	
+	glPushMatrix();
+		glColor3f(0.9f, 0.9f, 0.5f);
+		glScaled(0.8, 1.0, 0.5);
+		glTranslated(0.0, -0.5, 0.0);
+		glutSolidCube(3.0);
+	glPopMatrix();
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void idle() {
+	DWORD start = GetTickCount64();
+	while (GetTickCount64() - start < 1000 / FPS);
+
+	angle += 0.1;
+	if (angle > 360.0) angle = 0.0;
+
+	glutPostRedisplay();
+
+}
+
+void initGL() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glPointSize(10.0);
+	glLineWidth(5.0);
+
+	// camera setup
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrtho(-10, 10, -10, 10, 0.5, 1000.0);
+	gluPerspective(45.0, 1.0, 0.0001, 1000.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(5.0, 5.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	// lights
+	glEnable(GL_COLOR_MATERIAL); // cheat!!!!
+	glEnable(GL_LIGHTING);
+	GLfloat dir[] = { 0.2, 0.0, 1.0, 0.0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, dir);
+	glEnable(GL_LIGHT0);
+
+	// depth buffer
+	glEnable(GL_DEPTH_TEST);
+}
+
+int main(int argc, char** argv) {
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+	glutInit(&argc, argv);
+	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	glutCreateWindow("Test");
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	initGL();
+
+	glutMainLoop();
+
+}
