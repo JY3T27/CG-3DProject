d3265c2616ba7c1aa02298e8625c54c4fbe6d129
JY3T27 <121598846+JY3T27@users.noreply.github.com>
2023-06-22 14:36:14 +0800

    Create mic.cpp

---
+++ mic.cpp
@@ -0,0 +1,20 @@
+const float MICROPHONE_HEIGHT = 0.2f;
+const float MICROPHONE_RADIUS = 0.4f;
+
+void drawMicrophone() {
+	// Draw the microphone base (a cylinder)
+	glColor3f(0.5f, 0.5f, 0.5f);
+	GLUquadricObj* base = gluNewQuadric();
+	gluCylinder(base, MICROPHONE_RADIUS, MICROPHONE_RADIUS, MICROPHONE_HEIGHT * 0.3, 16, 1);
+
+	// Draw the microphone body (a cone)
+	glTranslatef(0.0f, 0.0f, MICROPHONE_HEIGHT * 0.2);
+	glColor3f(0.8f, 0.8f, 0.8f);
+	GLUquadricObj* body = gluNewQuadric();
+	gluCylinder(body, MICROPHONE_RADIUS * 0.7, 0.0, MICROPHONE_HEIGHT * 0.8, 16, 1);
+
+	// Draw the microphone top (a sphere)
+	glTranslatef(0.0f, 0.0f, MICROPHONE_HEIGHT * 0.8);
+	glColor3f(0.7f, 0.7f, 0.7f);
+	glutSolidSphere(MICROPHONE_RADIUS * 0.7, 16, 16);
+}
